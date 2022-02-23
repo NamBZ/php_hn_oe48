2f841a9ee2ce1b40a95b9b00e476a3f989c58580
NamBZ <nampt.me@gmail.com>
2022-02-23 10:58:32 +0700

    view book

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Rating;
 
 class ProductController extends Controller
 {
@@ -13,10 +14,19 @@ class ProductController extends Controller
      * @param  \App\Models\Product  $post
      * @return \Illuminate\Http\Response
      */
-    public function detail($slug)
+    public function show($slug)
     {
         $product = Product::where('slug', '=', $slug)->firstOrFail();
+        $ratings = $product->ratings()->paginate(config('pagination.rating_per_page'));
+        $related_products = Product::where('category_id', $product->category->id)
+            ->where('id', '!=', $product->id)
+            ->take(config('pagination.related'))
+            ->get();
 
-        return 1;
+        return view('products.details', [
+            'product' => $product,
+            'ratings' => $ratings,
+            'related_products' => $related_products,
+        ]);
     }
 }
--- config/pagination.php
+++ config/pagination.php
@@ -2,4 +2,6 @@
 
 return [
     'per_page' => 12,
+    'rating_per_page' => 5,
+    'related' => 6,
 ];
