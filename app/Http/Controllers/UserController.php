68a349fc495f857e20a7ff5e51a9f656d494a152
NamBZ <nampt.me@gmail.com>
2022-03-10 00:01:44 +0700

    view order history

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Enums\OrderStatus;
 
 class UserController extends Controller
 {
@@ -19,5 +21,45 @@ class UserController extends Controller
     public function index()
     {
         return view('dashboards.user.index');
+    }
+
+    public function purchase()
+    {
+        $user = Auth::user();
+        $orders = $user->orders()->paginate(config('pagination.per_page'));
+
+        return view('user.order.purchase', [
+            'user' => $user,
+            'orders' => $orders,
+        ]);
+    }
+
+    public function orderDetail($id)
+    {
+        $user = Auth::user();
+        $order = $user->orders()->with('orderItems')->with('shipping')->findOrFail($id);
+
+        return view('user.order.details', [
+            'user' => $user,
+            'order' => $order,
+        ]);
+    }
+
+    public function orderCancel(Request $request, $id)
+    {
+        $user = Auth::user();
+        $order = $user->orders()->findOrFail($id);
+
+        $order->status = OrderStatus::CANCELED;
+
+        if ($request->reason_canceled) {
+            $order->reason_canceled = $request->reason_canceled;
+        }
+
+        if ($order->save()) {
+            return redirect()->route('user.purchase')->with('success', __('Order is canceled'));
+        }
+
+        return redirect()->back()->with('error', __('Failed to cancel this order'));
     }
 }
--- resources/views/layouts/sidebarProfile.blade.php
+++ resources/views/layouts/sidebarProfile.blade.php
@@ -1,20 +1,24 @@
 @section('content')
 <div class="container rounded bg-white mt-2 mb-2">
     <div class="row">
-        <div class="col-md-3 border-right bg-light">
-            <div class="d-flex flex-column align-items-center text-center p-3 py-5">
-                <img class="rounded-circle" width="50px" src="{{ $user->avatar }}">
-                <span class="font-weight-bold">{{ $user->name }}</span>
-                <span> </span>
-            </div>
-            <div class="row mt-3 ps-2 h-100">
+        <div class="col-md-3">
+            <div class="shadow-lg rounded-3 p-2">
+                <div class="d-flex flex-column align-items-center text-center py-3">
+                    @if (empty($user->avatar))
+                        <i class="fa fa-user-circle-o fa-3x" aria-hidden="true"></i>
+                    @else
+                        <img class="rounded-circle" width="50px" src="{{ $user->avatar }}">
+                    @endif
+                    <span class="font-weight-bold">{{ $user->name }}</span>
+                    <span> </span>
+                </div>
                 <div class="list-group list-group-flush">
                     <a href="{{ route('profile') }}" class="list-group-item list-group-item-action">{{ __('My Account') }}</a>
-                    <a href="#" class="list-group-item list-group-item-action">{{ __('History Order') }}</a>
+                    <a href="{{ route('user.purchase') }}" class="list-group-item list-group-item-action">{{ __('History Order') }}</a>
                     <a href="#" class="list-group-item list-group-item-action">{{ __('My Review') }}</a>
                     <a href="#" class="list-group-item list-group-item-action">{{ __('My Wishlist') }}</a>
                 </div>
-                </div>
+            </div>
         </div>
         @yield('content2')
     </div>
